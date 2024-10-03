df91748bd16c23b4f5abed9b0e1c72f2dd99134d
pycoder-max <hellorishal@gmail.com>
2024-10-03 08:46:59 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,5 +1,19 @@
 import tkinter as tk
 
-root = tk.Tk()
+def loop():
+  canvas.delete("all")
+  root.after(20, loop)
 
-root.mainloop()
+root = tk.Tk()
+root.title("game")
+root.geometry("720x480")
+
+canvas = tk.Canvas(root,width = 720, 
+                   height = 480, 
+                   bg ="lightblue")
+canvas.pack()
+
+
+if __name__ == "main":
+  loop()
+  root.mainloop()
