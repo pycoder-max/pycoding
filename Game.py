91a2c4654aa8fc4f5f2724f66f050e3f91ee7303
pycoder-max <hellorishal@gmail.com>
2024-10-03 09:18:28 +0530

    Update Game with player.py

--- Game.py
+++ Game.py
@@ -1,7 +1,27 @@
 import tkinter as tk
 
+class Player:
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+    self.sy = 0
+    self.sx = 0
+    
+  def tick(self):
+    self.x += self.sx
+    self.y += self.sy
+    
+    self.sx *= 0.85
+    self.sy += 1
+    self.draw()
+  def draw(self):
+    canvas.create_rectangle(self.x,self.y,
+                            self.x+32,self.y+32,
+                            fill ="red")
+
 def loop():
   canvas.delete("all")
+  player.tick()
   root.after(20, loop)
 
 root = tk.Tk()
@@ -13,6 +33,7 @@ canvas = tk.Canvas(root,width = 720,
                    bg ="lightblue")
 canvas.pack()
 
+player = Player()
 
 if __name__ == "main":
   loop()
